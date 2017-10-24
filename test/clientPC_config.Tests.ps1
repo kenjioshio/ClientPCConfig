becd7d0293ea85ad31d126b33af55dff7af3a73c
kenjioshio <kenji_oshio@ot.olympus.co.jp>
2017-10-25 01:16:14 +0900

    This is a first commit.

---
+++ test/clientPC_config.Tests.ps1
@@ -0,0 +1,46 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+Describe "ClientPC_Config checking" {
+     Context "Check SVN Installation and Configuration" {
+        It "TortioseSVN folder should be exist in C:\Program Files" {
+        "C:\Program Files\TortoiseSVN" | Should Exist
+        }
+        It "TortoiseProc.exe should be exist in C:\Program Files\TortoiseSVN." {
+        "C:\Program Files\TortoiseSVN\bin\TortoiseProc.exe" | Should Exist
+        }
+        $result =$false
+        $envVal_Path = $env:Path
+        $envPathObj= $envVal_Path.split(";")
+        foreach($path in $envPathObj)
+        {
+            if($path -eq "C:\Program Files\TortoiseSVN\bin")
+            {
+                $result = $true
+                #Write-Host "C:\Program Files\TortoiseSVN\bin is exist as an Path Environment value "
+             }
+    }  
+        It "C:\Program Files\TortoiseSVN\bin should be exist in Path" {
+         $result | Should be $true
+        }
+    }
+
+     Context "Check NUnit Installation and Configuration" {
+        It "NUnit folder should be exist in C:\Program Files (x86)" {
+        "C:\Program Files (x86)\NUnit 2.6.4" | Should Exist
+        }
+        It "NUnit.exe should be exist in C:\Program Files (x86)\NUnit 2.6.4\bin." {
+            "C:\Program Files (x86)\NUnit 2.6.4\bin\nunit.exe" | Should Exist
+        }    
+     }
+
+     Context "Check Process Explorer Installation and Configuration" {
+        It "Process Explorer folder should be exist in C:\ProgramData\chocolatey\lib" {
+        "C:\ProgramData\chocolatey\lib\procexp" | Should Exist
+        }
+         It "Process Explorer.exe should be exist in C:\ProgramData\chocolatey\lib\procexp\tools." {
+        "C:\ProgramData\chocolatey\lib\procexp\tools\procexp.exe" | Should Exist
+        }
+     }
+}
---
+++ test/clientPC_config.ps1
@@ -0,0 +1,3 @@
+﻿function svn_config {
+
+}
