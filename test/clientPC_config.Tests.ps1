85c1b848dd02f6b9f3f79dd0afcdcf5710add108
kenjioshio <kenji_oshio@ot.olympus.co.jp>
2017-10-30 17:05:32 +0900

    Modify commets of test case, and add a test case.

--- test/clientPC_config.Tests.ps1
+++ test/clientPC_config.Tests.ps1
@@ -1,97 +1,116 @@
 ﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'. "$here\$sut"
 
-Describe "Checking whether Tools installation and the configuration of this PC are completed or not." {
+Describe ":Checking whether Tools installation and the configuration of this PC are completed or not." {
     #InstallされたTool類のチェック   
-    Context "Check 'SVN' Installation and Configuration" {
+    Context ":Check 'SVN' Installation and Configuration." {
         It "'TortoiseProc.exe' should be exist in 'C:\Program Files\TortoiseSVN'." {
             "C:\Program Files\TortoiseSVN\bin\TortoiseProc.exe" | Should Exist
         }
-        It "'TortoiseProc.exe' should be Version '$DesiredVersion'." {
+
             $DesiredVersion =$null
-            $DesiredVersion = "1.9.7.27907"
             #Version情報はpackage.configから取ってこれるとよいが、今はハードコーディングで実施。
+            $DesiredVersion = "1.9.7.27907"
+        It "'TortoiseProc.exe' should be'Ver.$DesiredVersion'." {
             $f_Version =(Get-ItemProperty "C:\Program Files\TortoiseSVN\bin\TortoiseProc.exe").VersionInfo.FileVersion
             $fileVersion = $f_Version.Replace(", ",".")       
             $fileVersion | Should be $DesiredVersion
         }
     }
-    Context "Check NUnit Installation and Configuration" {
+
+    Context ":Check 'NUnit' Installation and Configuration." {
         It "'NUnit.exe Ver2.6.4' should be exist in 'C:\Program Files (x86)\NUnit 2.6.4\bin'." {
             "C:\Program Files (x86)\NUnit 2.6.4\bin\nunit.exe" | Should Exist
         }
-        It "'NUnit.exe' should be 'Ver. $DesiredVersion'." {
+
             $DesiredVersion =$null
             #Version情報はpackage.configから取ってこれるとよいが、今はハードコーディングで実施。
             $DesiredVersion = "2.6.4"
+        It "'NUnit.exe' should be 'Ver.$DesiredVersion'." {
             #Chocolateyの仕様上、Install時のVersion指定とInstallされた後のRegistoryでVersionと桁が違うのでMatchで検証
             $f_Version =(Get-ItemProperty "C:\Program Files (x86)\NUnit 2.6.4\bin\nunit.exe").VersionInfo.FileVersion
             $fileVersion = $f_Version.Replace(", ",".")       
             $fileVersion | Should match $DesiredVersion
         }
-     }
-    Context "Check Process Explorer Installation and Configuration" {
-        It "'Process Explorer' folder should be exist in 'C:\ProgramData\chocolatey\lib'." {
-            "C:\ProgramData\chocolatey\lib\procexp" | Should Exist
-        }
+    }
+
+    Context ":Check 'Process Explorer' Installation and Configuration." {
         It "'Process Explorer.exe' should be exist in 'C:\ProgramData\chocolatey\lib\procexp\tools'." {
             #Chocolateyの仕様上、変なところにInstallされるが、PATHを設定すれば利用上問題ない。はず…
             "C:\ProgramData\chocolatey\lib\procexp\tools\procexp.exe" | Should Exist
         }
-     }
-    Context "Check 'OpenCover' Installation and Configuration" {
-        It "'OpenCover.Console.exe'should be exist in '$env:USERPROFILE\AppData\Local\Apps\OpenCover\OpenCover.Console.exe'." {
+            $DesiredVersion =$null
+            #Version情報はpackage.configから取ってこれるとよいが、今はハードコーディングで実施。
+            $DesiredVersion = "16.21"
+        It "'procexp.exe' should be 'Ver.$DesiredVersion'." {
+
+            #Chocolateyの仕様上、Install時のVersion指定とInstallされた後のRegistoryでVersionと桁が違うのでMatchで検証
+            $f_Version =(Get-ItemProperty "C:\ProgramData\chocolatey\lib\procexp\tools\procexp.exe").VersionInfo.FileVersion
+            $fileVersion = $f_Version.Replace(", ",".")       
+            $fileVersion | Should match $DesiredVersion
+
+    }
+    }
+
+    Context ":Check 'OpenCover' Installation and Configuration." {
+        It "'OpenCover.Console.exe'should be exist in '$env:USERPROFILE\AppData\Local\Apps\OpenCover'." {
             #Chocolateyの仕様上、変なところにInstallされるが、PATHを設定すれば利用上問題ない。はず…
             "$env:USERPROFILE\AppData\Local\Apps\OpenCover\OpenCover.Console.exe" | Should Exist
         }
-        It "'OpenCover.Console.exe' should be 'Ver. $DesiredVersion'." {
+        
             $DesiredVersion =$null
             #Version情報はpackage.configから取ってこれるとよいが、今はハードコーディングで実施。
             $DesiredVersion = "4.6.166"
+        It "'OpenCover.Console.exe' should be 'Ver.$DesiredVersion'." {
             #Chocolateyの仕様上、Install時のVersion指定とInstallされた後のRegistoryでVersionと桁が違うのでMatchで検証
             $f_Version =(Get-ItemProperty "$env:USERPROFILE\AppData\Local\Apps\OpenCover\OpenCover.Console.exe").VersionInfo.FileVersion
             $fileVersion = $f_Version.Replace(", ",".")       
             $fileVersion | Should match $DesiredVersion
         }
-     }
-    Context "Check 'ReportGenerator' Installation and Configuration" {
-        It "'ReportGenerator.exe'should be exist in 'C:\ProgramData\chocolatey\bin\ReportGenerator.exe'." {
+    }
+
+    Context ":Check 'ReportGenerator' Installation and Configuration." {
+        It "'ReportGenerator.exe'should be exist in 'C:\ProgramData\chocolatey\bin'." {
             #Chocolateyの仕様上、変なところにInstallされるが、PATHを設定すれば利用上問題ない。はず・・・
             "C:\ProgramData\chocolatey\bin\ReportGenerator.exe" | Should Exist
         }
-        It "'OpenCover.Console.exe' should be 'Ver. $DesiredVersion'." {
+        
             $DesiredVersion =$null
-            #Version情報はpackage.configから取ってこれるとよいが、今はハードコーディングで実施。
+            #Version情報はpackage.configから取ってこれるとよいが、今はハードコーディングで実施。   
             $DesiredVersion = "3.0.0.0" 
+        It "'OpenCover.Console.exe' should be 'Ver.$DesiredVersion'." {
             $f_Version =(Get-ItemProperty "C:\ProgramData\chocolatey\bin\ReportGenerator.exe").VersionInfo.FileVersion
             $fileVersion = $f_Version.Replace(", ",".")       
             $fileVersion | Should be $DesiredVersion
         }
-     }    
-    Context "Check '7zip' Installation and Configuration" {
-        It "7z.exe' should be exist in 'C:\Program Files\7-Zip\7z.exe'." {
+    }
+
+    Context ":Check '7zip' Installation and Configuration." {
+        It "'7z.exe' should be exist in 'C:\Program Files\7-Zip\7z.exe'." {
             "C:\Program Files\7-Zip\7z.exe" | Should Exist
         }
-        It "'7z.exe' should be 'Ver. $DesiredVersion'." {
             $DesiredVersion =$null
             #Version情報はEXEのFileVersionの表示上のDataと取得データが微妙に違うので、特別にハードコーディングで実施。
             $DesiredVersion = "16.04"
+        It "'7z.exe' should be 'Ver.$DesiredVersion'." {
             #Chocolateyの仕様上、Install時のVersion指定とInstallされた後のRegistoryでVersionと桁が違うのでMatchで検証
             $f_Version =(Get-ItemProperty "C:\Program Files\7-Zip\7z.exe").VersionInfo.ProductVersion
             $fileVersion = $f_Version.Replace(", ",".")       
-            $fileVersion | Should match $DesiredVersion
+            $fileVersion | Should be $DesiredVersion
         }
      }
 }
 
-Describe "Check Environment Variables of this PC" {
+Describe ":Check Environment Variables of this PC." {
     #$envVal_Path = $env:Path
-    #PCのプロパティーなどから取得する環境変数は変更後再起動しないと反映されないのでRegistoryから直接取得するのがBest(DSC configuration 実行後、RebootせずにPathが正しく設定されているか検証可能)      
+    #PCのプロパティーなどから取得する環境変数は変更後再起動しないと反映されないのでRegistoryから直接取得するのがBestかなと。
+    #(DSC configuration 実行後、RebootせずにPathが正しく設定されているか検証可能)      
     $envVal_Path =(Get-Item -Path "Registry::HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment").GetValue("Path")
-    #環境変数をファイルに落としてContainを使うとエスケープが面倒なので、時間はかかるが'；'で区切って、配列化し、オブジェクトとしてToolごとにループを回して比較。冗長で実行時間も長いがSimpleさを優先
+    #環境変数をファイルに落としてContainを使うとエスケープが面倒なので、時間はかかるが'；'で区切って、配列化し、オブジェクトとしてToolごとにループを回して比較。
+    #冗長で実行時間も多少かかるがSimpleさを優先
     $envPathObj= $envVal_Path.split(";")
 
-    Context "Check Evironment Variable 'Path' for SVN"{
+    Context ":Check Evironment Variable 'Path' for SVN."{
         It "'C:\Program Files\TortoiseSVN\bin' should be exist in environment variable 'Path'." {
             $result =$false
             foreach($path in $envPathObj){
@@ -103,7 +122,7 @@ Describe "Check Environment Variables of this PC" {
                 $result | Should be $true     
         }
     }   
-    Context "Chekc Evironment Variable 'Path' for NUnit"{
+    Context ":Chekc Evironment Variable 'Path' for NUnit."{
         It "'C:\Program Files (x86)\NUnit 2.6.4\bin' should be exist in environment variable 'Path'." {
             $result =$false
             foreach($path in $envPathObj){
